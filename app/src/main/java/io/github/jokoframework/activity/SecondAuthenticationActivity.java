98f6718d2a270169fa948a22fe5a4935480b84f0
Ale Elias <aleelias_92@hotmail.com>
2020-06-03 17:28:04 -0400

    Corregido algunos warnings y errores. Llamada al nuevo Home creado.

--- app/src/main/java/io/github/jokoframework/activity/SecondAuthenticationActivity.java
+++ app/src/main/java/io/github/jokoframework/activity/SecondAuthenticationActivity.java
@@ -6,19 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,19 +21,18 @@ import io.github.jokoframework.mboehaolib.util.Utils;
 
 public class SecondAuthenticationActivity extends BaseActivity {
     private EditText otpTextField;
-    private Button enterButton, cancelButton;
-    private Activity thisActivity;
     private String secret;
+    private Activity thisActivity;
     public Activity thisActivity() {
         return thisActivity;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         initializeUI();
         Bundle b = getIntent().getExtras();
+        assert b != null;
         secret = (String) b.get("SECRET");
     }
 
@@ -51,24 +43,16 @@ public class SecondAuthenticationActivity extends BaseActivity {
             Intent mServiceIntent = new Intent(getBaseContext(), io.github.jokoframework.service.CountryHelper.class);
             getBaseContext().startService(mServiceIntent);
         } catch (RuntimeException e) {
-            Utils.showToast(getBaseContext(), String.format("Fallo"));
+            Utils.showToast(getBaseContext(), "Fallo");
         }
 
-        otpTextField = (EditText) findViewById(R.id.otp);
-        findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getBaseContext(), "Back to Login", Toast.LENGTH_LONG).show();
-                finish();
-            }
+        otpTextField = findViewById(R.id.otp);
+        findViewById(R.id.cancel).setOnClickListener(v -> {
+            Toast.makeText(getBaseContext(), "Back to Login", Toast.LENGTH_LONG).show();
+            finish();
         });
 
-        findViewById(R.id.enter).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                otpValid(v);
-            }
-        });
+        findViewById(R.id.enter).setOnClickListener(this::otpValid);
     }
 
     private void otpValid(View view){
@@ -76,46 +60,39 @@ public class SecondAuthenticationActivity extends BaseActivity {
         String url = getString(R.string.user_acces_URL);
 
         final Context ctx = view.getContext();
-        Intent intent = new Intent(ctx, HomeActivity.class);
+        Intent intent = new Intent(ctx, Home2Activity.class);
 
-        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, null, response -> {
+            // Mostrar el response (DEBUG)
+            //Utils.showToast(getBaseContext(), String.format("Response is: "+ response.toString()));
 
-            @Override
-            public void onResponse(JSONObject response) {
-                // Mostrar el response (DEBUG)
-                //Utils.showToast(getBaseContext(), String.format("Response is: "+ response.toString()));
+            // Verificar login exitoso
+            String loginSuccess;
+            try {
+                loginSuccess = response.getString("success");
 
-                // Verificar login exitoso
-                String loginSuccess;
-                try {
-                    loginSuccess = response.getString("success");
+                if (loginSuccess.equals("true")){
+                    Toast.makeText(getBaseContext(), "Access Granted", Toast.LENGTH_LONG).show();
+                    ctx.startActivity(intent);
+                    ((Activity) ctx).finish();
+                } else {
+                    Utils.showToast(getBaseContext(), "Login failed.");
+                    showProgress(false);
+                    invalidOtp();
+                }
 
-                    if (loginSuccess.equals("true")){
-                        Toast.makeText(getBaseContext(), "Access Granted", Toast.LENGTH_LONG).show();
-                        ctx.startActivity(intent);
-                        ((Activity) ctx).finish();
-                    } else {
-                        Utils.showToast(getBaseContext(), String.format("Login failed."));
-                        showProgress(false);
-                        invalidOtp();
-                    }
-
-                } catch (Exception e){
-                    Log.e("Log_Tag", "Error at JWT Login " + e.getMessage(), e);
-                };
+            } catch (Exception e){
+                Log.e("Log_Tag", "Error at JWT Login " + e.getMessage(), e);
             }
-            }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                showProgress(false);
-                invalidOtp();
-                Log.d("Error", "error => " + error.toString());
-            }
+        }, error -> {
+            showProgress(false);
+            invalidOtp();
+            Log.d("Error", "error => " + error.toString());
         }){
             //Prepara el refresh_token como header para ser enviado como request
             @Override
-            public Map<String, String> getHeaders() throws AuthFailureError{
-                Map<String, String> params = new HashMap<String, String>();
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<>();
                 params.put("SEED_OTP_TOKEN", otpTextField.getText().toString());
                 params.put("X-JOKO-AUTH", secret);
                 return params;
